5643250b01e2c3494d32851929d2527772838acd
JorgeIsaacVilla <jvilla10@outlook.es>
2023-04-19 12:15:40 -0500

    header, secttion 1, menu header finsh.

--- js/script_menu.js
+++ js/script_menu.js
@@ -6,7 +6,7 @@ iconoMenu.addEventListener('click', (e) => {
     document.body.classList.toggle('opacity');
     const rutaActual = e.target.getAttribute('src');
     if(rutaActual == 'img/Cerrar.png'){
-        e.target.setAttribute('src','img/Grupo 14622@3x.png'); //falta poner la cruz no esta en el XD
+        e.target.setAttribute('src','./img/Opciones.png'); //falta poner la cruz no esta en el XD
     }else{
         e.target.setAttribute('src','img/Cerrar.png');
     }
