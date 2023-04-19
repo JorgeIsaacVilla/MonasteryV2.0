1d710d2661c63f82711c78dc22a88664c758e7e1
JorgeIsaacVilla <jvilla10@outlook.es>
2023-04-19 00:01:38 -0500

    ...Jorge Isaac Villa

--- js/script_menu.js
+++ js/script_menu.js
@@ -1,14 +1,14 @@
 window.onload = funciones
-const iconoMenu = document.querySelector('#icono-menu'),
+const iconoMenu = document.querySelector('#icon-menu'),
       menu = document.querySelector('#menu');
 iconoMenu.addEventListener('click', (e) => {
     menu.classList.toggle('active');
     document.body.classList.toggle('opacity');
     const rutaActual = e.target.getAttribute('src');
-    if(rutaActual == 'img/Grupo 14622@3x.png'){
+    if(rutaActual == 'img/Cerrar.png'){
         e.target.setAttribute('src','img/Grupo 14622@3x.png'); //falta poner la cruz no esta en el XD
     }else{
-        e.target.setAttribute('src','img/Grupo 14622@3x.png');
+        e.target.setAttribute('src','img/Cerrar.png');
     }
 });
 function funciones(){
