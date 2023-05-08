8928d002e0e3aaa15fb06ac6d8b2e7b6f0e9a090
JorgeIsaacVilla <jvilla10@outlook.es>
2023-05-08 17:24:18 -0500

    pagina 100% corregido, 3 versiones de roadmap, responsibe completo. se agrego un script para el boton de oradmap para que detecte la seccion de roadmap en version de escritorio. falta cambio de idioma.

--- js/script_road_desk_query.js
+++ js/script_road_desk_query.js
@@ -4,10 +4,8 @@ btnRoadmap.addEventListener("click", (event) => {
   event.preventDefault();
   
   if (window.innerWidth < 850) {
-    // Pantalla menor a 850px de ancho, redirigir a #Roadmap
     window.location.href = "#Roadmap";
   } else {
-    // Pantalla mayor o igual a 850px de ancho, redirigir a #Roadmap1
     window.location.href = "#Roadmap1";
   }
 });
