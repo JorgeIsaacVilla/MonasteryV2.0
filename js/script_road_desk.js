088695ee124b8a58751f3c2c6ae43fc7547368c0
JorgeIsaac <113570847+JorgeIsaacVilla@users.noreply.github.com>
2023-05-08 20:37:20 -0500

    Update script_road_desk.js

--- js/script_road_desk.js
+++ js/script_road_desk.js
@@ -5,14 +5,14 @@ const div3 = document.querySelector(".div3");
 const div4 = document.querySelector(".div4");
 
 div1.addEventListener("mouseover",()=>{
-    SectionQdeskvert.style.backgroundImage ="url(../img/Q_10.png)";
+    SectionQdeskvert.style.backgroundImage ="url(./img/Q_10.png)";
 });
 div2.addEventListener("mouseover",()=>{
-    SectionQdeskvert.style.backgroundImage ="url(../img/Q_20.png)";
+    SectionQdeskvert.style.backgroundImage ="url(./img/Q_20.png)";
 });
 div3.addEventListener("mouseover",()=>{
-    SectionQdeskvert.style.backgroundImage ="url(../img/Q_30.png)";
+    SectionQdeskvert.style.backgroundImage ="url(./img/Q_30.png)";
 });
 div4.addEventListener("mouseover",()=>{
-    SectionQdeskvert.style.backgroundImage ="url(../img/Q_40.png)";
+    SectionQdeskvert.style.backgroundImage ="url(./img/Q_40.png)";
 });
