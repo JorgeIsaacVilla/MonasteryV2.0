1600cd4207cfc7785e2b2d41bb3912299a8f2dcf
JorgeIsaacVilla <jvilla10@outlook.es>
2023-05-26 11:07:06 -0500

    actualitacion of section ofquestions, improvement of style in section ofquestions.

--- js/script_quez.js
+++ js/script_quez.js
@@ -21,38 +21,40 @@ const quezEn9 = document.querySelector("#quez_En9");
 const canvaAnswer = document.querySelector("#canva_answer");
 
 quezEs1.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/1.png" alt=""><p>Un NFT (Non Fungible Token), es un activo digital que representa objetos del mundo real tales como arte, música, objetos o elementos de videojuegos. Son comprados y vendidos en línea con criptomonedas, y generalmente son creados bajo la misma blockchain de algunas criptomonedas.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/1.png" alt=""><p>Un NFT (Non-Fungible Token) es un tipo de activo digital que representa la propiedad de un objeto único e irrepetible en una base de datos descentralizada llamada blockchain. Cada NFT tiene una identidad digital única y su valor depende de su autenticidad, rareza, popularidad y el mercado que lo respalda. Los NFT se han utilizado para representar arte digital, música, videos, juegos y otros objetos de colección digitales, y han ganado popularidad recientemente como una forma de monetizar y proteger la propiedad intelectual.</p>'
 });
 
 quezEs2.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/2.png" alt=""><p>Porque la cadena de bloques de Ethereum es la más usada, robusta, diversa y confiable, y la primera que usó los contratos inteligentes para sus transacciones. También, fue la cadena de bloques original para acuñar los primeros NFT, en 2014.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/2.png" alt=""><p>La blockchain de Ethereum es una de las plataformas más populares para la creación y distribución de colecciones de NFT debido a su funcionalidad de contrato inteligente, que permite a los creadores de NFTs establecer reglas y condiciones específicas para sus obras. Además, Ethereum ofrece una infraestructura descentralizada y segura, lo que significa que los NFTs pueden ser auténticos y no pueden ser falsificados o duplicados. Finalmente, Ethereum cuenta con una comunidad muy activa y una amplia gama de herramientas de desarrollo para facilitar la creación y el intercambio de NFTs.</p>'
 });
 
 quezEs3.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/3.png" alt=""><p>Crear (minting) un NFT es publicar, de manera original y única, el token en la cadena de bloques para poder venderlo. Imagina, acuñar una moneda y que al acuñar, el activo digital o archivo es almacenado en la cadena de bloques, de esta forma, puede ser comprado y vendido en cualquier mercado de criptos.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/3.png" alt=""><p>Acuñar un NFT (token no fungible) se refiere al proceso de crear una única unidad digital de un objeto de colección en particular, utilizando la tecnología blockchain. Esto implica crear un registro único de propiedad digital que se almacena en la cadena de bloques, lo que permite que los NFTs sean verificables, únicos y no intercambiables entre sí. La creación de un NFT se lleva a cabo a través de un contrato inteligente en una plataforma de blockchain, lo que permite que el objeto de colección pueda ser comprado, vendido o intercambiado en el mercado de NFTs.</p>'
 });
 
 quezEs4.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/4.png" alt=""><p>MetaMask es una billetera de criptomonedas que permite a los usuarios acceder al ecosistema de Web3 de aplicaciones descentralizadas (dapps). La MetaMask permite comprar, vender o transferir activos en la cadena de bloques. En resumen nos permite almacenar todas las compras y activos digitales.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/4.png" alt=""><p>Es una billetera digital que se utiliza para almacenar, enviar y recibir tokens y criptomonedas en la cadena de bloques Ethereum. Es una extensión de navegador que se puede utilizar con Chrome, Firefox y Brave o también, puedes descargarla en tu tienda de aplicaciones, para tu celular. Además de almacenar tokens y criptomonedas, Metamask también permite a los usuarios interactuar con aplicaciones descentralizadas (dApps) de Ethereum directamente desde su navegador web, sin necesidad de proporcionar sus claves privadas o contraseñas.</p>'
 });
 
 quezEs5.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/5.png" alt=""><p>Son las comisiones que debemos pagar al realizar una transacción a la red de ethereum, este pago de la comisión permite que la red pueda seguir funcionando y permita prestar un servicio transparente, ese fee es pagado a los mineros o servidores de la red.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/5.png" alt=""><p>Las tarifas (fees) por transacción en la blockchain se refieren a los costos asociados con el envío de una transacción en la red. Estos costos se pagan en la criptomoneda de la red (por ejemplo, Ethereum) y se utilizan para recompensar a los mineros o validadores de la red por verificar y procesar la transacción. Las tarifas varían según la demanda de la red y la complejidad de la transacción, y los usuarios pueden ajustar la cantidad de tarifas que pagan para acelerar el procesamiento de la transacción o para reducir los costos.</p>'
 });
 
 quezEs6.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/6.png" alt=""><p>Puedes ver el portafolio de Alejandro, incluyendo sus diseños NFT, en su sitio oficial: rmadesign.tv rmadesign.tv</p>'
+    canvaAnswer.innerHTML ='<img src="./img/6.png" alt=""><p>Para ver más del trabajo de RMA, se puede acceder al sitio web oficial de rmadesign.tv. Allí, se puede encontrar el portafolio de Alejandro, que incluye diseños NFT, colaboraciones y otros proyectos realizados por la empresa. Este sitio ofrece una forma conveniente y fácil de explorar el trabajo anterior de RMA y aprender más sobre su experiencia y habilidades en el campo del diseño y la tecnología.</p>'
 });
 
 quezEs7.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/7.png" alt=""><p>222 USD.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/7.png" alt=""><p>La gorra Gyatso NFT comenzó su venta el 22 de febrero de 2023 con un precio de mint de 0.135 ETH, lo que equivale aproximadamente a 222 dólares estadounidenses. Este fue el precio inicial establecido para la colección, que ofreció a los compradores la oportunidad de adquirir las gorras NFT exclusivas y únicas en la blockchain Ethereum.</p>'
 });
 
 quezEs8.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/8.png" alt=""><p>Puedes mintear y comprar un máximo de 6 gorras Gyatso.</p>'
+    canvaAnswer.innerHTML ='<img src="./img/9.png" alt=""> <ul><li>3 piezas de MONASTERY Esto incluye: GYATSO y dos prendas exclusivas </li><li>Dos NFT, uno para Decentraland y uno para la colección de RMA.</li><li>También, como parte del lujoso estilo de vida MST, invitaciones a fiestas que solo poseedores de la Gyatso VIP, podrán acceder.</li><li>Poder adquirir prendas de nuevas colecciones con anticipación.</li><li>Revisar todos los beneficios en el roadmap</li></ul>'
 });
 
 quezEs9.addEventListener("click",() =>{
-    canvaAnswer.innerHTML ='<img class="Image_acordeon" src="./img/9.png" alt=""><ul><li>3 piezas de MONASTERY Esto incluye: GYATSO y dos prendas random de la marca.</i><li>Dos NFT, uno para Decentraland y uno para la colección de RMA.</li><li>También, como parte del lujoso estilo de vida MST, invitaciones a fiestas que sólo poseedores de la Gyatso VIP, podrán acceder.</i><li>Poder adquirir las adquirir prendas de nuevas colecciones con anticipación.</li></ul>'
+    canvaAnswer.innerHTML ='<img src="./img/8.png" alt=""><p>Puedes comprar o vender el Nft Gyatso en cualquier plataforma, que soporte la red blockchain de Ethereum, Algunas de las opciones más populares son: </p><p class="acordeon-queztion-9">OpenSea: Es la plataforma de venta de NFT más grande y popular, con una amplia variedad de NFT disponibles y una gran base de usuarios.</p><p class="acordeon-queztion-9">Rarible: Esta plataforma también es popular y permite a los creadores de NFT vender, comprar y crear su propio contenido.</p><p class="acordeon-queztion-9">SuperRare: Una plataforma exclusiva de NFTs con un enfoque en arte digital de alta calidad y ediciones limitadas.</p>'
 });
+
+
 
